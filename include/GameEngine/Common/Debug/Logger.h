85ca40e241851c1e621486ae6bdd11a8b4af42c8
Kévin Seroux <kevin.seroux@orange.fr>
2013-07-27 05:17:34 +0200

    Add a evolved logger system

---
+++ include/GameEngine/Common/Debug/Logger.h
@@ -0,0 +1,75 @@
+/*------------------------------------------------------------------------------
+| This file is part of GameEngine.                                             |
+|                                                                              |
+| GameEngine is free software: you can redistribute it and/or modify it under  |
+| it under the terms of the GNU Lesser General Public License as published by  |
+| the Free Software Foundation, either version 3 of the License, or (at your   |
+| option) any later version.                                                   |
+|                                                                              |
+| GameEngine is distributed in the hope that it will be useful, but WITHOUT    |
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        |
+| FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public License |
+| for more details.                                                            |
+|                                                                              |
+| You should have received a copy of the GNU Lesser General Public License     |
+| along with GameEngine.  If not, see <http://www.gnu.org/licenses/>.          |
+\_____________________________________________________________________________*/
+
+#ifndef LOGGER__H
+#define LOGGER__H
+
+#include "GameEngine/Common/Config.h"
+#include "GameEngine/Common/SmartPtr.h"
+#include <iostream>
+#include <sstream>
+#include <cstdarg>
+#include <cstdio>
+
+namespace common
+{
+
+class GAME_ENGINE_EXPORT LoggerBase
+{
+public:
+	virtual void operator()(const char* const message, ...) = 0;
+
+private:
+	std::wostringstream m_stream;
+};
+
+struct GAME_ENGINE_EXPORT InfoLogger : public LoggerBase
+{
+	void operator()(const char* const message, ...);
+};
+
+struct GAME_ENGINE_EXPORT WarningLogger : public LoggerBase
+{
+	void operator()(const char* const message, ...);
+};
+
+struct GAME_ENGINE_EXPORT ErrorLogger : public LoggerBase
+{
+	void operator()(const char* const message, ...);
+};
+
+class GAME_ENGINE_EXPORT Logger
+{
+public:
+	static void setInfoLogger(LoggerBase* const logger);
+	static void setWarningLogger(LoggerBase* const logger);
+	static void setErrorLogger(LoggerBase* const logger);
+	static LoggerBase& logInfo();
+	static LoggerBase& logWarning();
+	static LoggerBase& logError();
+
+private:
+	Logger();
+
+    static SmartPtr<LoggerBase> m_infoLogger;
+    static SmartPtr<LoggerBase> m_warningLogger;
+    static SmartPtr<LoggerBase> m_errorLogger;
+};
+
+}
+
+#endif //LOGGER__H
---
+++ src/Common/Debug/Logger.cpp
@@ -0,0 +1,92 @@
+/*------------------------------------------------------------------------------
+| This file is part of GameEngine.                                             |
+|                                                                              |
+| GameEngine is free software: you can redistribute it and/or modify it under  |
+| it under the terms of the GNU Lesser General Public License as published by  |
+| the Free Software Foundation, either version 3 of the License, or (at your   |
+| option) any later version.                                                   |
+|                                                                              |
+| GameEngine is distributed in the hope that it will be useful, but WITHOUT    |
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        |
+| FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public License |
+| for more details.                                                            |
+|                                                                              |
+| You should have received a copy of the GNU Lesser General Public License     |
+| along with GameEngine.  If not, see <http://www.gnu.org/licenses/>.          |
+\_____________________________________________________________________________*/
+
+#include "GameEngine/Common/Debug/Logger.h"
+
+namespace common
+{
+
+SmartPtr<LoggerBase> Logger::m_infoLogger;
+SmartPtr<LoggerBase> Logger::m_warningLogger;
+SmartPtr<LoggerBase> Logger::m_errorLogger;
+
+void Logger::setInfoLogger(LoggerBase* const logger)
+{
+    m_infoLogger = logger;
+}
+
+void Logger::setWarningLogger(LoggerBase* const logger)
+{
+    m_warningLogger = logger;
+}
+
+void Logger::setErrorLogger(LoggerBase* const logger)
+{
+    m_errorLogger = logger;
+}
+
+LoggerBase& Logger::logInfo()
+{
+    if(m_infoLogger == 0)
+        m_infoLogger = new InfoLogger;
+    return *m_infoLogger;
+}
+
+LoggerBase& Logger::logWarning()
+{
+    if(m_warningLogger == 0)
+        m_warningLogger = new WarningLogger;
+    return *m_warningLogger;
+}
+
+LoggerBase& Logger::logError()
+{
+    if(m_errorLogger == 0)
+        m_errorLogger = new ErrorLogger;
+    return *m_errorLogger;
+}
+
+void InfoLogger::operator()(const char* const message, ...)
+{
+	va_list pa;
+	va_start(pa, message);
+	vprintf(message, pa);
+	std::cout << std::endl;
+	va_end(pa);
+}
+
+void WarningLogger::operator()(const char* const message, ...)
+{
+	va_list pa;
+	va_start(pa, message);
+	printf("[WARNING] ");
+	vprintf(message, pa);
+	std::cout << std::endl;
+	va_end(pa);
+}
+
+void ErrorLogger::operator()(const char* message, ...)
+{
+	va_list pa;
+	va_start(pa, message);
+	printf("[ERROR] ");
+	vprintf(message, pa);
+	std::cout << std::endl;
+	va_end(pa);
+}
+
+}
