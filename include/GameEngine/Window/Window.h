8bc18f3e95864adc1689ab1c37b585e8197e390f
Kévin Seroux <kevin.seroux@orange.fr>
2013-08-05 16:54:56 +0200

    Add the window creation attributes
    Add the possibility to directly create a OpenGL window

--- include/GameEngine/Window/Window.h
+++ include/GameEngine/Window/Window.h
@@ -24,10 +24,13 @@
 #include "GameEngine/Common/Config.h"
 
 #ifdef WINDOWS_SYSTEM
-    #include "GameEngine/Window/Win32/Win32Window.h"
+	#include "GameEngine/Window/Win32/WGL/WGLContext.h"
 #elif defined(LINUX_SYSTEM)
-    #include "GameEngine/Window/X/XWindow.h"
+	#include "GameEngine/Window/X/GLXContext.h"
 #endif
+
+//Forward declaration
+typedef int16_t;
 
 namespace window
 {
@@ -39,7 +42,8 @@ namespace window
     /// \brief When you compile on Windows RenderWindow is an alias to create a
     ///        Win32 window
     ////////////////////////////////////////////////////////////////////////////
-    typedef Win32Window RenderWindow;
+    typedef Win32Window Window;
+	typedef WGLContext GLRenderWindow;
 
 #elif defined(LINUX_SYSTEM)
 
@@ -48,10 +52,29 @@ namespace window
     /// \brief When you compile on Linux RenderWindow is an alias to create a
     ///        X.org window
     ////////////////////////////////////////////////////////////////////////////
-    typedef XWindow RenderWindow;
+    typedef XWindow Window;
+	typedef GLXContext GLRenderWindow;
 
 #endif
 
+    ////////////////////////////////////////////////////////////////////////////
+    /// \brief Contain all the parameters to create a window
+    ////////////////////////////////////////////////////////////////////////////
+    struct WindowAttributes
+    {
+		bool show;
+
+        wchar_t* title;
+		char* iconURL;
+		bool displayCursor;
+        Window* parent;
+		unsigned char openGLVersion;
+        bool fullScreen;
+        int16_t posX;
+        int16_t posY;
+        int16_t width;
+        int16_t height;
+    };
 }
 
 #endif // WINDOW__H
